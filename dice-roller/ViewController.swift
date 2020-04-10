23134ff0be4a35692e26237ecd8c4dabead12cca
mikedilulo <mikedilulo2020@gmail.com>
2020-04-10 14:52:41 -0600

    Random Dice Images Appear on Button Click

--- dice-roller/ViewController.swift
+++ dice-roller/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceViewImageOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "diceOne"), #imageLiteral(resourceName: "diceTwo"), #imageLiteral(resourceName: "diceThree"), #imageLiteral(resourceName: "diceFour"), #imageLiteral(resourceName: "diceFive"), #imageLiteral(resourceName: "diceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func changeDiceWhenPressed(_ sender: UIButton) {
+        diceViewImageOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+    }
+    
 
 }
 
